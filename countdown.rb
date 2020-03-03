1ca2d2051f8ed04fd15801f36b9ac7b6cc7931da
Sajid Anjum <sajid1760@gmail.com>
2020-03-03 21:14:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,4 +10,16 @@ end
 "HAPPY NEW YEAR!"
 end
 
-countdown(5)
+def countdown_with_sleep(number)
+  while (number > 1)
+  puts "#{number} seconds!"
+  sleep(1)
+  number -= 1
+end
+if (number == 1) then puts "1 second!"
+end
+sleep(1)
+"HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(5)
