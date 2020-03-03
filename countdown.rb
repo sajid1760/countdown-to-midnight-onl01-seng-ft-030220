f94e0ed8b3ac005084f9a1242fc9ae9c28aca940
Sajid Anjum <sajid1760@gmail.com>
2020-03-03 21:17:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,24 +1,19 @@
 
 
 def countdown(number)
-  while (number > 1)
-  puts "#{number} seconds!"
+  while (number > 0)
+  puts "#{number} SECOND(S)!"
   number -= 1
-end
-if (number == 1) then puts "1 second!"
 end
 "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  while (number > 1)
-  puts "#{number} seconds!"
+  while (number > 0)
+  puts "#{number} SECOND(S)!"
   sleep(1)
   number -= 1
 end
-if (number == 1) then puts "1 second!"
-end
-sleep(1)
 "HAPPY NEW YEAR!"
 end
 
